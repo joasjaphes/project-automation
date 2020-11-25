788a9262b1984d986ebf20b4243197245862e6b7
joas japhes <joasjaphes665@gmail.com>
2020-11-25 10:40:12 +0300

    added command for changing remote url

--- .custom_commands.sh
+++ .custom_commands.sh
@@ -1,4 +1,8 @@
 #! usr/bin/sh
+
+# command to initialize projects, 
+# it create local repo as well as remote repo and push initial commit,
+# it also create readme.md file
 function create () {
     # to do add logic for cloning remote repo...............
     # git@github.com:Jcom2019/meals_app.git
@@ -14,11 +18,31 @@ function create () {
     mkdir $1
     cd $1
     git init
-    git remote add origin git@github.com:$2/$1.git
+    git remote add origin git@github.com:$username/$1.git
     touch Readme.md
     git add .
     git commit -m 'initial commit'
     git push origin master
     code .
     echo $1
+}
+#command to change remote url for your local repositories having common remote,
+function changeRemote () {
+    cd
+    cd joas/projects
+    for dir in */
+    do
+        # echo $dir
+        cd $dir
+        result=`pwd`
+        remote=`git remote -v`
+        if echo "$remote" | grep -q "Jcom2019"; then
+            dirname=`basename $result`
+            echo $dirname
+            git remote set-url origin git@github.com:joasjaphes/$dirname.git
+            echo "$remote"
+        fi
+        # echo $dirname
+        cd ../
+    done
 }
